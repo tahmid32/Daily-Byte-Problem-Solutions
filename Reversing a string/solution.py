7314d0f8e17bc295119bcdf6ad26ed57876269bc
tahmid32 <64402489+tahmid32@users.noreply.github.com>
2022-06-25 01:40:42 +0600

    Update solution.py

--- Reversing a string/solution.py
+++ Reversing a string/solution.py
@@ -1,1 +1,15 @@
-
+def reverse_string(str):
+    char_lst = []
+    for char in str:
+        char_lst.append(char)
+    
+    start, stop = 0, (len(char_lst) - 1)
+    while (start < stop):
+        char_lst[start], char_lst[stop] = char_lst[stop], char_lst[start]
+        start, stop = start+1, stop-1
+    
+    final_str = ""
+    for char in char_lst:
+        final_str += char
+        
+    return final_str
