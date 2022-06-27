09c282418a5e630571aa7276dcf1b9eb1bce6976
tahmid32 <64402489+tahmid32@users.noreply.github.com>
2022-06-28 01:38:30 +0600

    Update solution.py

--- Valid Palindrome/solution.py
+++ Valid Palindrome/solution.py
@@ -1,16 +1,12 @@
 def valid_palindrome(str):
     char_lst = []
-    isPalindrome = True
     
     for char in str:
         if (char.isalpha() == True):
             char_lst.append(char)
             
     for i in range(len(char_lst) // 2):
-        if (char_lst[i].lower() == char_lst[-(i + 1)].lower()):
-            continue
-        else:
-            isPalindrome = False
-            break
-        
-    return isPalindrome
+        if (char_lst[i].lower() != char_lst[-(i + 1)].lower()):
+            return False
+            
+    return True
