0014e5cf8f73b29d1e50f5ff2fe5fd41d7391303
tahmid32 <64402489+tahmid32@users.noreply.github.com>
2022-07-31 21:16:29 +0600

    Update solution.py

--- First Unique Character/solution.py
+++ First Unique Character/solution.py
@@ -1,5 +1,7 @@
 def first_unique(string):
     count_dict = {}
+    first_unique_char = ''
+    
     for char in string:
         count_dict[char] = count_dict.get(char, 0) + 1
           
@@ -7,5 +9,8 @@ def first_unique(string):
         if (value == 1):
             first_unique_char = key
             break
-        
-    return string.index(first_unique_char)
+    
+    if (first_unique_char == ''):
+        return -1
+    else:
+        return string.index(first_unique_char)
